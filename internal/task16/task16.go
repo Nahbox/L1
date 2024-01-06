673f315a444951bcd335637c4fb2c1b41779ebc7
Idris Akkulov <i.akkulov@gmail.com>
2024-01-06 16:27:05 +0300

    task16

---
+++ internal/task16/task16.go
@@ -0,0 +1,37 @@
+package task16
+
+import "fmt"
+
+func Task16() {
+	set := []int{1, 7, 4, 12, 3, 2, 9, 1, 5, 4, 3, 11, 15}
+	quickSortStart(set)
+
+	fmt.Println(set)
+}
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func quickSortStart(arr []int) []int {
+	return quickSort(arr, 0, len(arr)-1)
+}
