002150ef1bec56d82349171acf9dfbf96636c02a
Idris Akkulov <i.akkulov@gmail.com>
2024-01-07 16:27:35 +0300

    task24

---
+++ internal/task24/task24.go
@@ -0,0 +1,47 @@
+package task24
+
+import (
+	"fmt"
+	"math"
+)
+
+func Task24() {
+	var x1, y1 float64
+	fmt.Print("Введите координату x для точки 1: ")
+	fmt.Scan(&x1)
+	fmt.Print("Введите координату y для точки 1: ")
+	fmt.Scan(&y1)
+
+	var x2, y2 float64
+	fmt.Print("Введите координату x для точки 2: ")
+	fmt.Scan(&x2)
+	fmt.Print("Введите координату y для точки 2: ")
+	fmt.Scan(&y2)
+
+	// Создаем две точки
+	point1 := NewPoint(x1, y1)
+	point2 := NewPoint(x2, y2)
+
+	// Вычисляем расстояние между точками
+	distance := point1.Distance(point2)
+
+	// Выводим результат
+	fmt.Printf("Расстояние между точками: %.2f\n", distance)
+}
+
+type Point struct {
+	x, y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{
+		x,
+		y,
+	}
+}
+
+func (p *Point) Distance(other *Point) float64 {
+	dx := p.x - other.x
+	dy := p.y - other.y
+	return math.Sqrt(math.Pow(dx, 2) + math.Pow(dy, 2))
+}
