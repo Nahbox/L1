c0676b4f92067db807e0c6c8224a0b480e7a5fc2
Idris Akkulov <i.akkulov@gmail.com>
2024-01-07 12:21:52 +0300

    task23

---
+++ internal/task23/task23.go
@@ -0,0 +1,35 @@
+package task23
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+func Task23() {
+	// Пример использования
+	mySlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println("Original Slice:", mySlice)
+
+	// Удаление элемента по индексу
+	mySlice, err := removeElement(mySlice, 3)
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Println("Slice after removal:", mySlice)
+}
+
+func removeElement(slice []int, index int) ([]int, error) {
+	// Проверка на корректность индекса
+	if index < 0 || index >= len(slice) {
+		return slice, errors.New("Invalid index")
+	}
+
+	// Перераспределение слайса
+	copy(slice[index:], slice[index+1:])
+	slice = slice[:len(slice)-1]
+
+	return slice, nil
+}
