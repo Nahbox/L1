c4449e0b91a3f692f8b9ee8cca01fded595a1d34
Idris Akkulov <i.akkulov@gmail.com>
2024-01-09 12:36:52 +0300

    add comments

--- internal/task1/task1.go
+++ internal/task1/task1.go
@@ -11,17 +11,25 @@ type Human struct {
 	name string
 }
 
+// Метод sayName выводит имя человека.
 func (h Human) sayName() {
 	fmt.Println("My name is", h.name)
 }
 
+// Метод powAge выводит квадрат возраста человека.
 func (h Human) powAge() {
 	fmt.Println(h.age * h.age)
 }
 
+// Функция Task1 для выполнения задачи.
 func Task1() {
+	// Создание объекта типа Human с возрастом 20 и именем "John".
 	human := Human{age: 20, name: "John"}
+	// Создание объекта типа Action, включающего в себя объект Human.
 	action := Action{Human: human}
+
+	// Вызов метода powAge для вывода квадрата возраста.
 	action.powAge()
+	// Вызов метода sayName для вывода имени.
 	action.sayName()
 }
